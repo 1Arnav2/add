fee5929c45585f115baa07f385dde2c07868aea6
1Arnav2 <arnav.ambre2003@gmail.com>
2024-04-30 01:40:48 +0530

    Initial commit of Addition Java program

---
+++ add.java
@@ -0,0 +1,8 @@
+public class add {
+    public static void main(String[] args) {
+        int number1 = 5; // You can change these numbers
+        int number2 = 3; // You can change these numbers
+        int sum = number1 + number2;
+        System.out.println("Sum: " + sum);
+    }
+}
