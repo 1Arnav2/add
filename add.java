57dce1f1c6956fef6516de778757e53c26e6d365
1Arnav2 <108128094+1Arnav2@users.noreply.github.com>
2024-04-30 12:52:07 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,7 +1,7 @@
 public class add {
     public static void main(String[] args) {
-        int number1 = 5; // You can change these numbers
-        int number2 = 3; // You can change these numbers
+        int number1 = 5; 
+        int number2 = 3; 
         int sum = number1 + number2;
         System.out.println("Sum: " + sum);
     }
